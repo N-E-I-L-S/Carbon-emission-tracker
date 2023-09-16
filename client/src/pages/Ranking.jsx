bac6bf1822e2854db061a85c64157f54cd854ed3
Neil Shukla <neilpshukla221203@gmail.com>
2023-09-17 02:01:11 +0530

    ranking created

--- client/src/pages/Ranking.jsx
+++ client/src/pages/Ranking.jsx
@@ -1,34 +1,55 @@
 import React, { useEffect, useState } from 'react'
+import GoldT from '../assets/gold.png'
+import SilverT from '../assets/silver.png'
+import BronzeT from '../assets/bronze.png'
 
 export default function Ranking() {
-    const [user, setUser] = useState([])
+    const [data, setData] = useState([])
+    const [top, setTop] = useState([])
+    const [loading, setLoading] = useState(true)
     async function GetRequest() {
         const res = await fetch('http://localhost:3001/api/getalluser')
         const json = await res.json();
-        setUser(json)
-        console.log(json)
+        const sortedData = json.sort((a, b) => a.emission - b.emission);
+        const top = sortedData.slice(0,3);
+        setTop(top)
+        setData(sortedData);
+        console.log(top)
+        setLoading(false)
     }
 
-    const [data, setData] = useState([])
-    function SetGraphData(){
-        setData(user.sort((a, b) => b.emission - a.emission))
-    }
+    // function SetGraphData(){
+    //     setData(user.sort((a, b) => b.emission - a.emission))
+    // }
 
     useEffect(() => {
         GetRequest()
-        SetGraphData()
+        // SetGraphData()
     }, [])
+    if(!loading)
   return (
     <>
     <div className="">
-       {data.map((i, index)=>{
-        console.log(i)
-        return(
-            <div className="" key={index}>
-                {i.username}
-            </div>
-        )
-        })}
+      <div className="flex flex-col p-4 w-[80%] mt-20">
+        <div className="flex mt-4">
+        <img src={GoldT} alt="" style={{height: "4.5rem", marginRight: "2rem"}} /> 
+        <div className="w-[100%]  border-white bg-blue-500 py-6 pl-4 ">
+           {top[0].username}
+        </div>
+        </div>
+        <div className="flex mt-4">
+        <img src={SilverT} alt="" style={{height: "4.5rem", marginRight: "2rem"}} />
+        <div className="w-[80%]  border-white bg-blue-300 py-6 pl-4 ">
+            {top[1].username}
+        </div>
+        </div>
+        <div className="flex mt-4">
+            <img src={BronzeT} alt="" style={{height: "4.5rem", marginRight: "2rem"}} />
+        <div className="w-[60%] border-white bg-blue-100 py-6 pl-4 ">
+            {top[2].username}
+        </div>
+        </div>
+      </div>
     </div>
     </>
   )
