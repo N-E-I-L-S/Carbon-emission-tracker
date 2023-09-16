29c6d8520a381ead6967da85f849a9427f217d27
Neil Shukla <neilpshukla221203@gmail.com>
2023-09-16 16:55:39 +0530

    server added

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -23,51 +23,51 @@ export default function LoginPage() {
             setLogged1(false);
         }
     };
-    useEffect(()=>{
+    useEffect(() => {
         console.log(user)
-    },[])
-    if(user==null)
-    return (
-        <>
-            <div className="login-page">          
-                <form onSubmit={(e) => {
-                    e.preventDefault();
-                    handleLogin(uname.current.value, pwd.current.value)
-                }}
-                >
-                    <div className="login-div">
-                      <h5 className='mb-4'>Kindly Login To Add Blog</h5>                        
-                        {logged ?
-                            <p className='pmatch'>Wrong Credentials</p>
-                            :
-                            null
-                        }
-                        {logged1 ?
-                            <p className='pmatch-1'>Logged In!</p>
-                            :
-                            null
-                        }
-         
-                        <div className="username">
-                            {/* <label htmlFor="">Username</label> <br /> */}
-                            <input placeholder='Username' type="email" id="uname" ref={uname} required={true} />
+    }, [])
+    if (user == null)
+        return (
+            <>
+                <div className="login-page">
+                    <form onSubmit={(e) => {
+                        e.preventDefault();
+                        handleLogin(uname.current.value, pwd.current.value)
+                    }}
+                    >
+                        <div className="login-div">
+                            <h5 className='mb-4'>Kindly Login To Add Blog</h5>
+                            {logged ?
+                                <p className='pmatch'>Wrong Credentials</p>
+                                :
+                                null
+                            }
+                            {logged1 ?
+                                <p className='pmatch-1'>Logged In!</p>
+                                :
+                                null
+                            }
+
+                            <div className="username">
+                                {/* <label htmlFor="">Username</label> <br /> */}
+                                <input placeholder='Username' type="email" id="uname" ref={uname} required={true} />
+                            </div>
+                            <div className="password">
+                                {/* <label htmlFor="">Password</label> <br /> */}
+                                <input placeholder='Password' type="password" id="pwd" ref={pwd} required={true} />
+                            </div>
+
+                            <div className="btns">
+                                <button type='submit' className='blogcardbtn'>Login</button>
+                            </div>
+                            <div className='login-options my-3' style={{ marginLeft: "25vw" }}>
+                                <NavLink className="td" to='/signup'>Don't have an account?  </NavLink> <br />
+                            </div>
                         </div>
-                        <div className="password">
-                            {/* <label htmlFor="">Password</label> <br /> */}
-                            <input placeholder='Password' type="password" id="pwd" ref={pwd} required={true} />
-                        </div>
-                        
-                        <div className="btns">
-                            <button type='submit' className='blogcardbtn'>Login</button>
-                        </div>
-                        <div className='login-options my-3' style={{ marginLeft: "25vw" }}>
-                        <NavLink className="td" to='/signup'>Don't have an account?  </NavLink> <br/>
-            </div>
-                    </div>
-                </form>
-            </div>
-        </>
-    )
+                    </form>
+                </div>
+            </>
+        )
     else
-    navigate("/home")
+        navigate("/home")
 }
---
+++ server/models/DataSchema.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const DataSchema = new Schema({
+        
+});
+module.exports = mongoose.model('data', DataSchema)
